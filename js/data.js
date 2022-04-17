4499cb36bac3c1b2eb4be00a23c19abf0536b900
Sezer Berber <56473728+nyperu@users.noreply.github.com>
2022-04-17 13:42:04 +0300

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,17 @@
+var veriler = [
+    {
+        "adi": "Sevmiyorum",
+        "turu": "docx",
+        "boyutu": "50"
+     },
+     {
+        "adi": "Webi",
+        "turu": "exe",
+        "boyutu": "50"
+     },
+     {
+        "adi": "Sevemiyorum.",
+        "turu": "exe",
+        "boyutu": "150"
+     },
+];
